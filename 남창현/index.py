daa5d16f92526e82b464d4fd6db8a39d0ee599cc
Hepheir <hepheir@gmail.com>
2017-06-09 16:32:14 +0900

    .
    (cherry picked from commit b6113dc7a08070d6f5945b80f513d7921fe71537)

--- 남창현/index.py
+++ 남창현/index.py
@@ -1,49 +1,54 @@
 from bs4 import BeautifulSoup
 import requests
 
-from operator import itemgetter # sort용
+from operator import itemgetter
 
-keyword = input('검색어를 입력해주세요.\n>>> ')
+keyword = input('검색어를 입력하세요.\n>>>')
 url = 'http://www.google.co.kr/search?q='+keyword
 
 html = requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
-
 data = ''
 
-# 구글은 `span.st`에 검색 결과 내용이 출력되길래 `st`클래스명을 가진 `span`태그를 읽어들이는 부분임.
-for span_tag in soup.find_all('span', { 'class' : 'st' }):
+for span_tag in soup.find_all( 'span', { 'class' : 'st' } ):
     text = span_tag.text
     text = text.replace('.', '').replace(',', '')
-
     data = data + text + ' '
 
-
-# 이제 자주 나오는 단어들을 찾아 볼 거임.
-
-used_words = dict() # 딕셔너리 자료형
-
+used_words = dict()
 length = len(data) - 1
+parsed_list = []
 
-for i in range(0, length, 1):
+f = open('list.txt','r')
+
+while True:
+    line = f.readline()
+
+    if not line: break
+
+    line = line.replace('\n', '')
+    parsed_list.append(line)
+
+for i in range (0, length, 1):
     word = ''
 
-    # 띄어쓰기를 기준으로 단어를 구분 할 거임.
-    while data[i] != ' ' and i < length :
-        word = word + data[i]
+    while data[i] != ' ' and i < length:
+        word += data[i]
         i += 1
-    
-    # 이미 추가했던 단어면 값만 +1 해줌
+
     if word in used_words:
         used_words[word] += 1
         continue
-    
-    # 새로운 단어 추가
-    if word != '':
-        used_words[word] = 0
 
-sorted_words = sorted(used_words.items(), key = itemgetter(1), reverse = True)
+    if len(word) > 1 and word in parsed_list:
+        used_words[word] = 1
+        
+sorted_words = sorted( used_words.items(), key = itemgetter(1), reverse = True)
 
-print("\n('단어', 사용빈도)\n")
-for i in range (0, 6):
-    print(sorted_words[i])
+if sorted_words:
+    print("\n('단어',사용빈도)\n")
+    print(sorted_words)
+else:
+    print('사용된 단어 중 위키백과 5000단어와 일치하는 단어가 없습니다.')
+
+f.close()
